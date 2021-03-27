0edcaaf1a4db4cded79ba47d44508aa204210eb6
sunseeker <2891978553@qq.com>
2021-03-27 16:15:20 +0800

    更新 持久层

--- src/main/java/com/paint/demo/Service/Impl/printerOrderServiceImpl.java
+++ src/main/java/com/paint/demo/Service/Impl/printerOrderServiceImpl.java
@@ -2,5 +2,5 @@ package com.paint.demo.Service.Impl;
 
 import com.paint.demo.Service.printerOrderService;
 
-public class printerOrdeerServiceImpl implements printerOrderService {
+public class printerOrderServiceImpl implements printerOrderService {
 }
